2d23d2e3cd153b0d59803fc2bd75ac468516665b
petrovich601 <114338320+petrovich601@users.noreply.github.com>
2024-03-03 16:18:58 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App; 
